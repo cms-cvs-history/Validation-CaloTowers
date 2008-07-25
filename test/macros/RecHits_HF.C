0eab74f5689295436cd194ba1d74c7494926888f
Salavat Abdoulline <sha1-8e38bffa4b6141c3b05a505ddf6b2603102043d9@cern.ch>
2008-07-25 08:50:43 +0000

    added stat boxes

--- test/macros/RecHits_HF.C
+++ test/macros/RecHits_HF.C
@@ -3,8 +3,8 @@
 
    TCanvas *myc = new TCanvas("myc","",800,600);
       
-   TFile f1("HcalRecHitValidationHF_206.root");
-   TFile f2("HcalRecHitValidationHF_210pre6.root");
+   TFile f1("HcalRecHitValidationHF_210pre6.root");
+   TFile f2("HcalRecHitValidationHF_210pre9.root");
 
    
    // service variables
@@ -67,8 +67,9 @@
 
    for (int i = 0; i < Nhist1; i++){
 
-    f1_hist1[i]->SetStats(kFALSE);   
-    f2_hist1[i]->SetStats(kFALSE); 
+     if(i <= 1 )   f1_hist1[i]->SetStats(kTRUE);   // stat
+     else  f1_hist1[i]->SetStats(kFALSE);
+
     
     f1_hist1[i]->SetTitle("");
     f2_hist1[i]->SetTitle("");
@@ -78,20 +79,37 @@
     f1_hist1[i]->SetLineWidth(2); 
     f1_hist1[i]->SetLineStyle(1); 
     
-    f2_hist1[i]->SetLineColor(42);
+    f2_hist1[i]->SetLineColor(43);
     f2_hist1[i]->SetLineWidth(3);     
     f2_hist1[i]->SetLineStyle(2);  
 
 
-    TLegend *leg = new TLegend(0.68, 0.82, 0.98, 0.97, "","brNDC");
+    TLegend *leg = new TLegend(0.58, 0.87, 0.84, 0.97, "","brNDC");
     leg->SetBorderSize(2);
     //  leg->SetFillColor(51); // see new color definition above
     leg->SetFillStyle(1001); 
-    leg->AddEntry(f1_hist1[i],"CMSSW_206","l");
-    leg->AddEntry(f2_hist1[i],"CMSSW_210pre6","l");
+    leg->AddEntry(f1_hist1[i],"CMSSW_210pre6","l");
+    leg->AddEntry(f2_hist1[i],"CMSSW_210pre9","l");
 
-    f1_hist1[i]->Draw("hist");   
-    f2_hist1[i]->Draw("hist same");   
+
+     if (i<= 1) {
+       TPaveStats *ptstats = new TPaveStats(0.85,0.86,0.98,0.98,"brNDC");
+       ptstats->SetTextColor(41);
+       f1_hist1[i]->GetListOfFunctions()->Add(ptstats);
+       ptstats->SetParent(f1_hist1[i]->GetListOfFunctions());
+       TPaveStats *ptstats = new TPaveStats(0.85,0.74,0.98,0.86,"brNDC");
+       ptstats->SetTextColor(43);
+       f2_hist1[i]->GetListOfFunctions()->Add(ptstats);
+       ptstats->SetParent(f2_hist1[i]->GetListOfFunctions());
+
+       f1_hist1[i]->Draw(""); // "stat"
+       f2_hist1[i]->Draw("hist sames");
+     }
+     else {
+       f1_hist1[i]->Draw(""); // "stat"
+       f2_hist1[i]->Draw("hist same");
+     }
+
     
     leg->Draw();   
     
@@ -143,8 +161,8 @@
      TLegend *leg = new TLegend(0.38, 0.82, 0.68, 0.97, "","brNDC");    
      leg->SetBorderSize(2);
      leg->SetFillStyle(1001); 
-     leg->AddEntry(f1_hist2[i],"CMSSW_206","l");
-     leg->AddEntry(f2_hist2[i],"CMSSW_210pre6","l");
+     leg->AddEntry(f1_hist2[i],"CMSSW_210pre6","l");
+     leg->AddEntry(f2_hist2[i],"CMSSW_210pre9","l");
      
      leg->Draw();   
      
