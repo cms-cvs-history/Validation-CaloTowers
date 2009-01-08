082ef3796a25df6c1c5306e2bf3efd3f9833384a
Salavat Abdoulline <sha1-8e38bffa4b6141c3b05a505ddf6b2603102043d9@cern.ch>
2009-01-08 15:41:58 +0000

    removing (redundant) geometry inludes

--- test/runHF_gamma_valid_simhits_digis_rechits_towers_cfg.py
+++ test/runHF_gamma_valid_simhits_digis_rechits_towers_cfg.py
@@ -1,19 +1,16 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("VALID")
-process.load("Validation.CaloTowers.Validation_cfi")
+process.load("Validation.CaloTowers.Validation_hcalonly_cfi")
 
 process.PoolSource.fileNames = ['file:/afs/cern.ch/cms/data/CMSSW/Validation/HcalHits/data/1_4_x/mc_gamma50_hf-.root', 'file:/afs/cern.ch/cms/data/CMSSW/Validation/HcalHits/data/1_4_x/mc_gamma50_hf+.root']
 
-process.hcalDigiAnalyzer.outputFile = 'HcalDigisValidationHF_gamma.root'
-process.hcalDigiAnalyzer.hcalselector = 'HF'
-
 process.hcalRecoAnalyzer.outputFile = 'HcalRecHitValidationHF_gamma.root'
 process.hcalRecoAnalyzer.hcalselector = 'HF'
-process.hcalRecoAnalyzer.ecalselector = 'yes'
+process.hcalRecoAnalyzer.ecalselector = 'no'
 process.hcalRecoAnalyzer.eventype = 'single'
 
-process.hcalTowerAnalyzer.outputFile = 'CaloTowersValidationHF.root'
-process.hcalTowerAnalyzer.hcalselector = 'HF'
+#process.p = cms.Path(process.VtxSmeared*process.g4SimHits*process.mix*process.calDigi*process.calolocalreco*process.caloTowersRec*process.hcalDigiAnalyzer*process.hcalRecoAnalyzer*process.hcalTowerAnalyzer)
 
-process.p = cms.Path(process.VtxSmeared*process.g4SimHits*process.mix*process.calDigi*process.calolocalreco*process.caloTowersRec*process.hcalDigiAnalyzer*process.hcalRecoAnalyzer*process.hcalTowerAnalyzer)
+process.p = cms.Path(process.VtxSmeared * process.g4SimHits * process.mix * process.simHcalUnsuppressedDigis * process.simHcalDigis * process.hbhereco* process.horeco * process.hfreco * process.hcalRecoAnalyzer)
+
