d19cdab08c172167e5bbb615360f0f5642f1899c
Salavat Abdoulline <sha1-8e38bffa4b6141c3b05a505ddf6b2603102043d9@cern.ch>
2009-06-15 10:18:38 +0000

    additional standalone config for the entire noise change up to CaloTowers

---
+++ test/runNoise_valid_simhits_digis_rechits_calotowers_NZS_cfg.py
@@ -0,0 +1,109 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("TEST")
+
+### RANDOM setting (change last digit(s) to make runs different !)
+process.load("Configuration.StandardSequences.SimulationRandomNumberGeneratorSeeds_cff")
+#process.RandomNumberGeneratorService.generator.initialSeed = 12345XXXX
+
+process.load("Configuration.StandardSequences.Simulation_cff")
+process.load("Configuration.StandardSequences.MixingNoPileUp_cff")
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+process.load('Configuration/StandardSequences/DigiToRaw_cff')
+process.load('Configuration/StandardSequences/RawToDigi_cff')
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = 'IDEAL_31X::All'
+
+process.load("Configuration.StandardSequences.VtxSmearedGauss_cff")
+process.load("Configuration.StandardSequences.GeometryECALHCAL_cff")
+process.load("Configuration.StandardSequences.MagneticField_cff")
+process.g4SimHits.UseMagneticField = False
+
+process.load("DQMServices.Core.DQM_cfg")
+process.DQM.collectorHost = ''
+
+process.options = cms.untracked.PSet(
+    Rethrow = cms.untracked.vstring('ProductNotFound')
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    firstEvent = cms.untracked.uint32(1),
+    debugFlag = cms.untracked.bool(True),
+    debugVebosity = cms.untracked.uint32(2),
+    noEventSort = cms.untracked.bool(True),	
+    duplicateCheckMode = cms.untracked.string("noDuplicateCheck"),
+    fileNames = cms.untracked.vstring(
+'file:/afs/cern.ch/cms/data/CMSSW/Validation/HcalHits/data/3_1_X/mc_nue.root'
+    )
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1000)
+)
+
+
+process.hcalDigiAnalyzer = cms.EDFilter("HcalDigiTester",
+    digiLabel = cms.InputTag("simHcalUnsuppressedDigis"),
+    outputFile = cms.untracked.string('HcalDigisValidationHB.root'),
+    hcalselector = cms.untracked.string('HB'),
+    zside = cms.untracked.string('*')
+)
+
+process.hcalRecoAnalyzer = cms.EDFilter("HcalRecHitsValidation",
+    outputFile = cms.untracked.string('HcalRecHitsValidation.root'),
+    eventype = cms.untracked.string('single'),
+    mc = cms.untracked.string('yes'),
+    sign = cms.untracked.string('*'),
+    hcalselector = cms.untracked.string('noise'),
+    ecalselector = cms.untracked.string('no')
+)
+
+process.hcalTowerAnalyzer = cms.EDFilter("CaloTowersValidation",
+    outputFile = cms.untracked.string('CaloTowersValidation.root'),
+    CaloTowerCollectionLabel = cms.untracked.string('towerMaker'),
+    hcalselector = cms.untracked.string('all')
+)
+
+#------------------------------------
+
+process.simHcalDigis.HBlevel = -1000
+process.simHcalDigis.HElevel = -1000
+process.simHcalDigis.HOlevel = -1000
+process.simHcalDigis.HFlevel = -1000
+
+
+process.VtxSmeared.SigmaX = 0.00001
+process.VtxSmeared.SigmaY = 0.00001
+process.VtxSmeared.SigmaZ = 0.00001
+
+
+### Special - CaloOnly ---------------------------------------------------
+process.ecalGlobalUncalibRecHit.EBdigiCollection = cms.InputTag("simEcalDigis","ebDigis")
+process.ecalGlobalUncalibRecHit.EEdigiCollection = cms.InputTag("simEcalDigis","eeDigis")
+process.ecalPreshowerRecHit.ESdigiCollection = cms.InputTag("simEcalPreshowerDigis") 
+process.hbhereco.digiLabel = cms.InputTag("simHcalDigis")
+process.horeco.digiLabel   = cms.InputTag("simHcalDigis")
+process.hfreco.digiLabel   = cms.InputTag("simHcalDigis")
+process.ecalRecHit.recoverEBIsolatedChannels = cms.bool(False)
+process.ecalRecHit.recoverEEIsolatedChannels = cms.bool(False)
+process.ecalRecHit.recoverEBFE = cms.bool(False)
+process.ecalRecHit.recoverEEFE = cms.bool(False)
+
+
+process.g4SimHits.Generator.HepMCProductLabel = 'generator'
+process.p = cms.Path(
+ process.mix *
+ process.calDigi *
+ process.ecalPacker *
+ process.esDigiToRaw *
+ process.hcalRawData *
+ process.rawDataCollector *
+ process.ecalDigis * 
+ process.ecalPreshowerDigis * 
+ process.hcalDigis *
+ process.calolocalreco *
+ process.caloTowersRec *
+ process.hcalRecoAnalyzer *
+ process.hcalTowerAnalyzer
+)
