f4e29dce3c189ec3d1531294996a30490f90d038
Salavat Abdoulline <sha1-8e38bffa4b6141c3b05a505ddf6b2603102043d9@cern.ch>
2011-02-01 16:09:57 +0000

    adding 4 depths ieta-iphi maps of HCAL RecHits <E> "val" and "ref" plots are compared side-by-side

--- test/run_onRelVal_mc_cfg.py
+++ test/run_onRelVal_mc_cfg.py
@@ -23,7 +23,15 @@ process.maxEvents = cms.untracked.PSet(
 
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-
+       '/store/relval/CMSSW_3_11_0/RelValTTbar/GEN-SIM-RECO/MC_311_V1_64bit-v2/0012/E4D9AB0D-B72B-E011-98B3-0030487C2B86.root',
+       '/store/relval/CMSSW_3_11_0/RelValTTbar/GEN-SIM-RECO/MC_311_V1_64bit-v2/0011/F2D37EC6-282B-E011-95DC-001D09F2527B.root',
+       '/store/relval/CMSSW_3_11_0/RelValTTbar/GEN-SIM-RECO/MC_311_V1_64bit-v2/0011/D234573A-282B-E011-AA46-001D09F291D2.root',
+       '/store/relval/CMSSW_3_11_0/RelValTTbar/GEN-SIM-RECO/MC_311_V1_64bit-v2/0011/C2690CD4-272B-E011-AE30-0030487A18D8.root',
+       '/store/relval/CMSSW_3_11_0/RelValTTbar/GEN-SIM-RECO/MC_311_V1_64bit-v2/0011/B8987305-292B-E011-88C2-001D09F23A20.root',
+       '/store/relval/CMSSW_3_11_0/RelValTTbar/GEN-SIM-RECO/MC_311_V1_64bit-v2/0011/B44D0C89-292B-E011-AE7A-001D09F2AD4D.root',
+       '/store/relval/CMSSW_3_11_0/RelValTTbar/GEN-SIM-RECO/MC_311_V1_64bit-v2/0011/42EDCA5B-292B-E011-B5AB-001D09F24FBA.root',
+       '/store/relval/CMSSW_3_11_0/RelValTTbar/GEN-SIM-RECO/MC_311_V1_64bit-v2/0011/3E80EEB0-262B-E011-92ED-0030487A195C.root',
+       '/store/relval/CMSSW_3_11_0/RelValTTbar/GEN-SIM-RECO/MC_311_V1_64bit-v2/0011/144AA7E6-292B-E011-A567-001D09F23A20.root'
       ),
     inputCommands = cms.untracked.vstring('keep *', 'drop *_MEtoEDMConverter_*_*')
 )
